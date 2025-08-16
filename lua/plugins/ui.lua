d2fc69bcc5de99f0e403f22ff06a508da43fc61c
Miles Possing <mp-complete@pm.me>
2025-08-15 20:52:50 -0500

    Fixing name

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,7 +1,7 @@
 return {
   {
     'catppuccin/nvim',
-    as = 'catppuccin',
+    name = 'catppuccin-nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       vim.cmd.colorscheme 'catppuccin-macchiato'
