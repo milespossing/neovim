1dbc4eed800cc34006550953e5232c4510e857a1
Miles Possing <milespossing@microsoft.com>
2025-08-26 17:47:22 +0000

    Fix for blink

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,7 +1,7 @@
 return {
   {
     'saghen/blink.cmp',
-    enabled = require('nixCatsUtils').enableForCategory('full'),
+    enabled = require('nixCatsUtils').enableForCategory 'full',
     version = '*',
     event = 'InsertEnter',
     dependencies = {
@@ -18,26 +18,26 @@ return {
       completion = {
         documentation = { auto_show = false },
         ghost_text = { enabled = true },
-      },
-      menu = {
-        direction_priority = function()
-          local ctx = require('blink.cmp').get_context()
-          local item = require('blink.cmp').get_selected_item()
-          if ctx == nil or item == nil then
+        menu = {
+          direction_priority = function()
+            local ctx = require('blink.cmp').get_context()
+            local item = require('blink.cmp').get_selected_item()
+            if ctx == nil or item == nil then
+              return { 's', 'n' }
+            end
+
+            local item_text = item.textEdit ~= nil and item.textEdit.newText or item.insertText or item.label
+            local is_multi_line = item_text:find '\n' ~= nil
+
+            -- after showing the menu upwards, we want to maintain that direction
+            -- until we re-open the menu, so store the context id in a global variable
+            if is_multi_line or vim.g.blink_cmp_upwards_ctx_id == ctx.id then
+              vim.g.blink_cmp_upwards_ctx_id = ctx.id
+              return { 'n', 's' }
+            end
             return { 's', 'n' }
-          end
-
-          local item_text = item.textEdit ~= nil and item.textEdit.newText or item.insertText or item.label
-          local is_multi_line = item_text:find '\n' ~= nil
-
-          -- after showing the menu upwards, we want to maintain that direction
-          -- until we re-open the menu, so store the context id in a global variable
-          if is_multi_line or vim.g.blink_cmp_upwards_ctx_id == ctx.id then
-            vim.g.blink_cmp_upwards_ctx_id = ctx.id
-            return { 'n', 's' }
-          end
-          return { 's', 'n' }
-        end,
+          end,
+        },
       },
       sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot', 'git' },
