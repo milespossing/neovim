520038d0ccc689527af39939b227d47dc17a57c7
Miles Possing <miles@nixos.localdomain>
2025-12-16 17:59:22 +0000

    Test updates

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -8,6 +8,25 @@ return {
       'antoinemadec/FixCursorHold.nvim',
       'nvim-treesitter/nvim-treesitter',
     },
+    config = function(_, opts)
+      if opts.adapters then
+        local adapters = {}
+        for name, config in pairs(opts.adapters) do
+          local adapter = require(name)
+          if type(config) == 'table' and not vim.tbl_isempty(config) then
+            if adapter.setup then
+              adapter.setup(config)
+            elseif adapter.adapter then
+              adapter.adapter(config)
+              adapter = adapter.adapter
+            end
+          end
+          adapters[#adapters + 1] = adapter
+        end
+        opts.adapters = adapters
+      end
+      require('neotest').setup(opts)
+    end,
   },
   {
     'nvim-neotest/neotest',
